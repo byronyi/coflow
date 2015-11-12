b22c6f14db9e3f8a4c4642b87c82b18fca351953
byronyi <yibairen1994@gmail.com>
2015-11-12 14:01:37 +0800

    cleanup

---
+++ src/main/java/com/github/byronyi/CoflowSelectorProvider.java
@@ -0,0 +1,47 @@
+package com.github.byronyi;
+
+import sun.nio.ch.DefaultSelectorProvider;
+
+import java.io.IOException;
+import java.net.ProtocolFamily;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.Pipe;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.AbstractSelector;
+import java.nio.channels.spi.SelectorProvider;
+
+public class CoflowSelectorProvider extends SelectorProvider {
+
+    final SelectorProvider selectorProvider = DefaultSelectorProvider.create();
+
+    @Override
+    public DatagramChannel openDatagramChannel() throws IOException {
+        return selectorProvider.openDatagramChannel();
+    }
+
+    @Override
+    public DatagramChannel openDatagramChannel(ProtocolFamily family) throws IOException {
+        return selectorProvider.openDatagramChannel(family);
+    }
+
+    @Override
+    public Pipe openPipe() throws IOException {
+        return selectorProvider.openPipe();
+    }
+
+    @Override
+    public AbstractSelector openSelector() throws IOException {
+        return selectorProvider.openSelector();
+    }
+
+    @Override
+    public SocketChannel openSocketChannel() throws IOException {
+        return new CoflowSocketChannel(this);
+    }
+
+    @Override
+    public ServerSocketChannel openServerSocketChannel() throws IOException {
+        return new CoflowServerSocketChannel(this);
+    }
+}
---
+++ src/main/java/com/github/byronyi/CoflowServerSocketChannel.java
@@ -0,0 +1,103 @@
+package com.github.byronyi;
+
+import sun.nio.ch.SelChImpl;
+import sun.nio.ch.SelectionKeyImpl;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.SocketAddress;
+import java.net.SocketOption;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
+
+class CoflowServerSocketChannel extends ServerSocketChannel implements SelChImpl {
+
+    final private ServerSocketChannel serverSocketChannel;
+
+    protected CoflowServerSocketChannel(CoflowSelectorProvider provider) throws IOException {
+        super(provider);
+        serverSocketChannel = provider.selectorProvider.openServerSocketChannel();
+    }
+
+    @Override
+    public ServerSocketChannel bind(SocketAddress local, int backlog) throws IOException {
+        serverSocketChannel.bind(local, backlog);
+        return this;
+    }
+
+    @Override
+    public <T> ServerSocketChannel setOption(SocketOption<T> name, T value) throws IOException {
+        return serverSocketChannel.setOption(name, value);
+    }
+
+    @Override
+    public <T> T getOption(SocketOption<T> name) throws IOException {
+        return serverSocketChannel.getOption(name);
+    }
+
+    @Override
+    public Set<SocketOption<?>> supportedOptions() {
+        return serverSocketChannel.supportedOptions();
+    }
+
+    @Override
+    public ServerSocket socket() {
+        return serverSocketChannel.socket();
+    }
+
+    @Override
+    public SocketChannel accept() throws IOException {
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        if (socketChannel == null) {
+            return null;
+        }
+        return new CoflowSocketChannel(this.provider(), socketChannel);
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() throws IOException {
+        return serverSocketChannel.getLocalAddress();
+    }
+
+    @Override
+    protected void implCloseSelectableChannel() throws IOException {
+        serverSocketChannel.close();
+    }
+
+    @Override
+    protected void implConfigureBlocking(boolean block) throws IOException {
+        serverSocketChannel.configureBlocking(block);
+    }
+
+    @Override
+    public FileDescriptor getFD() {
+        return ((SelChImpl) serverSocketChannel).getFD();
+    }
+
+    @Override
+    public int getFDVal() {
+        return ((SelChImpl) serverSocketChannel).getFDVal();
+    }
+
+    @Override
+    public boolean translateAndUpdateReadyOps(int i, SelectionKeyImpl selectionKey) {
+        return ((SelChImpl) serverSocketChannel).translateAndUpdateReadyOps(i, selectionKey);
+    }
+
+    @Override
+    public boolean translateAndSetReadyOps(int i, SelectionKeyImpl selectionKey) {
+        return ((SelChImpl) serverSocketChannel).translateAndSetReadyOps(i, selectionKey);
+    }
+
+    @Override
+    public void translateAndSetInterestOps(int i, SelectionKeyImpl selectionKey) {
+        ((SelChImpl) serverSocketChannel).translateAndSetInterestOps(i, selectionKey);
+    }
+
+    @Override
+    public void kill() throws IOException {
+        ((SelChImpl) serverSocketChannel).kill();
+    }
+}
